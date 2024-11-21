28fffeea6ffe91c09a5faa6a4c589379170e1eaa
Mariangel34 <141246195+Mariangel34@users.noreply.github.com>
2024-11-20 21:21:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Go (ou Golang)?',
+    'O Go (ou Golang) é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Venezuela?',
+    'A capital da Venezuela é Caracas'
+)
+
+criaCartao(
+    'Programação',
+    'O que são laços de repetição (loops)?',
+    'são estruturas de controle fundamentais em programação, que permitem a execução repetida de um bloco de código enquanto uma condição for verdadeira, ou por um número específico de iterações.'
+)
+
+criaCartao(
+    'Linguagem Espanhola',
+    'Como se diz Boa Noite em Espanhol?',
+    'Boa Noite é "Buenas Noches".'
+)
